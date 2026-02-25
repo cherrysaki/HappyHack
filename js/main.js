a7fffa64c156b3fcc6811908d2f4b87f8717ccbb
Saki <ksaki0402@gmail.com>
2026-02-25 16:05:16 +0900

    レスポンシブ対応など微調整

--- js/main.js
+++ js/main.js
@@ -18,6 +18,20 @@ $(function () {
     slidesToShow: 4,
     slidesToScroll: 1,
     infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 3, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 2, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 480,
+        settings: { slidesToShow: 1, slidesToScroll: 1 },
+      },
+    ],
   });
 
   particlesJS("particles-js", {
