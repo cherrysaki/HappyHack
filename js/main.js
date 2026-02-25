bd1faa6ec0e3c94cea530973646a684f4bbad9c7
Saki <ksaki0402@gmail.com>
2026-02-25 14:50:17 +0900

    particle.jsを追加

--- js/main.js
+++ js/main.js
@@ -19,4 +19,75 @@ $(function () {
     slidesToScroll: 1,
     infinite: true,
   });
+
+  particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#ffffff",
+      },
+      shape: {
+        type: "circle",
+      },
+      opacity: {
+        value: 0.5,
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 100,
+        color: "#ffffff",
+        opacity: 0.8,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 8,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+      },
+    },
+    // interactivity: {
+    //   detect_on: "canvas",
+    //   events: {
+    //     onhover: {
+    //       enable: true,
+    //       mode: "repulse",
+    //     },
+    //     onclick: {
+    //       enable: true,
+    //       mode: "push",
+    //     },
+    //     resize: true,
+    //   },
+    //   modes: {
+    //     repulse: {
+    //       distance: 100,
+    //       duration: 0.4,
+    //     },
+    //     push: {
+    //       particles_nb: 4,
+    //     },
+    //   },
+    // },
+    retina_detect: true,
+  });
 });
